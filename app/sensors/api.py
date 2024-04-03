9a98d344d06b43650c3add1e8207a8b76bb8d36d
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2024-04-03 23:44:07 +0200

    Improve api calls for temperatures (previously called CPU)

--- app/sensors/api.py
+++ app/sensors/api.py
@@ -1,23 +1,28 @@
 import psutil
-from fastapi import APIRouter
-from fastapi.exceptions import HTTPException
+from fastapi import APIRouter, Request
+
+from .models import TemperatureDevice, TemperatureSensor
 
 router = APIRouter()
 
 
-@router.get("/cpu")
-def list_available_cpu_devices():
-    return list(psutil.sensors_temperatures().keys())
+@router.get("/temperature")
+def list_available_cpu_devices(request: Request):
+    sensors = [
+        TemperatureDevice(
+            device_name=device,
+            paths=[f"{request.url}/{device}/{i}" for i, _ in enumerate(sub_devices)],
+        )
+        for device, sub_devices in psutil.sensors_temperatures().items()
+    ]
+    return sensors
 
 
-@router.get("/cpu/{device}")
-@router.get("/cpu/{device}/{core}")
-def read_cpu_temperature(device, core: int = None):
-    cpu_temp = psutil.sensors_temperatures()[device]
-    if core is None:
-        return cpu_temp
-
-    if core < 0 or core >= len(cpu_temp):
-        raise HTTPException(404, f"Core {core} not found (available: {len(cpu_temp)})")
-
-    return cpu_temp[core]
+@router.get("/temperature/{device}")
+@router.get("/temperature/{device}/{sub_device}")
+def read_cpu_temperature(device, sub_device: int = None):
+    sensors = psutil.sensors_temperatures()[device]
+    if sub_device is not None:
+        sensor = sensors[sub_device]
+        return TemperatureSensor(name=sensor[0], value=sensor[1])
+    return [TemperatureSensor(name=sensor[0], value=sensor[1]) for sensor in sensors]
