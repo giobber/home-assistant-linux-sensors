02321093ed2f5be0e10cd9846acc0a28b3e3b21c
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2024-05-23 23:11:09 +0200

    Add loop cli commands

--- app/cli.py
+++ app/cli.py
@@ -1,6 +1,30 @@
+import time
+
 import typer
 
 import app.sensors.cli
 
 cli = typer.Typer()
 cli.add_typer(app.sensors.cli.cli, name="sensors")
+
+
+@cli.command()
+def run(device: str, sensor: str, wait: int = 1):
+    """Continuous read of temperatures"""
+    import psutil
+
+    while True:
+        temperatures = psutil.sensors_temperatures()
+
+        sensors: list
+        if not (sensors := temperatures.get(device)):
+            typer.echo("Device not found")
+            return
+
+        if sensor not in map(lambda s: s.label, sensors):
+            typer.echo("Sensor not found")
+            return
+
+        typer.echo(next(s.current for s in sensors if s.label == sensor))
+
+        time.sleep(wait)
